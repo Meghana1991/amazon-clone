c24c71487feb428596bbdc4c7f202dabacfdfb37
Anand Kumar <Meghana.AnandKumar@Honeywell.com>
2020-10-30 07:51:43 +0530

    FIrst Commit from HW laptop

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBepen3KL9TplrjfH97pxjXhU63VTpf92w",
+  authDomain: "clone-3add4.firebaseapp.com",
+  databaseURL: "https://clone-3add4.firebaseio.com",
+  projectId: "clone-3add4",
+  storageBucket: "clone-3add4.appspot.com",
+  messagingSenderId: "69707783733",
+  appId: "1:69707783733:web:957f7d2a27a93bda25db7c",
+  measurementId: "G-BYSXCQ5XPQ"
+};
